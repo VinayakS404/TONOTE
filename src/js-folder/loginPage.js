d459f9a5a95abb5eee48f605059836034ace5f60
Vinayak S <oreva.vinayak@gmail.com>
2026-01-25 22:44:01 +0530

    feat: add hidden password feature

---
+++ src/js-folder/loginPage.js
@@ -0,0 +1,12 @@
+const passwordInpElement = document.querySelector(".js-password-inp");
+const eyeIconElement = document.querySelector(".js-eye-icon");
+
+eyeIconElement.addEventListener("click", () => {
+  if (passwordInpElement.type === "password") {
+    passwordInpElement.setAttribute("type", "text");
+    eyeIconElement.src = "./img/hidden.png";
+  } else if (passwordInpElement.type === "text") {
+    passwordInpElement.setAttribute("type", "password");
+    eyeIconElement.src = "./img/eye-icon.png";
+  }
+});
