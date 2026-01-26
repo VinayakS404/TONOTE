51fe7b5a3b0b33bf574d77f804d9374374119d41
Vinayak S <oreva.vinayak@gmail.com>
2026-01-26 23:47:13 +0530

    ...

--- src/js-folder/usernameNPassword.js
+++ src/js-folder/usernameNPassword.js
@@ -1,1 +1,6 @@
 const userData = {};
+const passwordInpElement = document.querySelector(".js-password-inp");
+const eyeIconElement = document.querySelector(".js-eye-icon");
+const confirmPasswordInpElement = document.querySelector(
+  ".js-confirm-password-inp",
+);
