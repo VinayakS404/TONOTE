060f7a2193a7213ddcd69133054bbaafb4861c07
Vinayak S <oreva.vinayak@gmail.com>
2026-02-14 23:16:50 +0530

    feat: enhance todo item click handling to trigger edit page and manage active states

--- src/js-folder/homeMenu.js
+++ src/js-folder/homeMenu.js
@@ -166,16 +166,13 @@ nowBtn.addEventListener("click", () => {
   window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
 });
 taskContainer.addEventListener("click", (event) => {
-  // Find the closest parent that is a todo item
   const todoElement = event.target.closest(".js-task-day-single-with-date-div");
   
   if (todoElement) {
     const todoId = Number(todoElement.dataset.todoName);
     
-    // 1. Trigger the edit page
     triggerEditpage(todoId);
     
-    // 2. Handle visual "active" states
     document.querySelectorAll(".js-task-day-single-with-date-div").forEach(div => {
        div.classList.remove("task-day-single-with-date-div-active");
     });
